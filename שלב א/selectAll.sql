db6f57bb61620dcad04d81c34e08519dd4f7641c
אוריאן גייסמן <155296760+auriyan@users.noreply.github.com>
2025-04-22 17:49:55 +0300

    Create selectAll.sql

---
+++ שלב א/selectAll.sql
@@ -0,0 +1,12 @@
+-- selectAll.sql
+
+SELECT * FROM customer;
+SELECT * FROM employee;
+SELECT * FROM instructor;
+SELECT * FROM cashier;
+SELECT * FROM equipment;
+SELECT * FROM lesson;
+SELECT * FROM rental;
+SELECT * FROM payment;
+SELECT * FROM lesson_participants;
+SELECT * FROM rental_equipment;
